6598a03036387e0dd6d18f3a8bc694e7004f8d49
Amnasaqib <>
2024-01-08 04:01:28 +0000

    Name appointments

--- run.py
+++ run.py
@@ -234,28 +234,28 @@ def get_time(data):
 
 
              def get_avail_times(data):
-     """
-     Gets return value from get_appts_for_date function for booked times
-     and removes them from the appointment times list to create a list of
-     available times and returns the available times. If the current date
-     is input, past times are removed from available times.
-     """
-     appt_times = ["0800",
-                   "0900",
-                   "1000",
-                   "1100",
-                   "1200",
-                   "1400",
-                   "1500",
-                   "1600"
+    """
+    Gets return value from get_appts_for_date function for booked times
+    and removes them from the appointment times list to create a list of
+    available times and returns the available times. If the current date
+    is input, past times are removed from available times.
+    """
+    appt_times = ["0800",
+                  "0900",
+                  "1000",
+                  "1100",
+                  "1200",
+                  "1400",
+                  "1500",
+                  "1600"
                    ]
-     unav_times = get_appts_for_date(data, "booked_times")
-     av_times = [time for time in appt_times if time not in unav_times]
-     if data == CURRENT_DATE_FMTED:
+    unav_times = get_appts_for_date(data, "booked_times")
+    av_times = [time for time in appt_times if time not in unav_times]
+    if data == CURRENT_DATE_FMTED:
          current_time = datetime.datetime.now().strftime("%H%M")
          today_av_times = [time for time in av_times if time > current_time]
          return today_av_times
-     else:
+    else:
          return av_times
 
     
@@ -407,3 +407,20 @@ def search_name(reason):
          return [search_nme, name_appts, name_recs]
     else:
          display_records(name_recs, name_desc, name_heads)
+
+
+def get_appts_for_name(name):
+    """
+    Gets and returns the appointments booked for the name
+    provided as an argument.
+    """
+    all_appts = APPTS.get_all_values()
+    name_appts = []
+    for appt in all_appts[1:]:
+         appt_name = appt[2:4]
+         if appt_name == name:
+             name_appt_row = [all_appts.index(appt) + 1]
+             name_appt = appt + name_appt_row
+             name_appts.append(name_appt)
+
+    return name_appts 
